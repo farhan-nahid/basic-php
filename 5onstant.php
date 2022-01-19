42419ab5f6d127e1c182a323d6cc2d799d243633
gesora96 <nahidahmed9933@gmail.com>
2022-01-20 00:51:08 +0600

    constants added

---
+++ 5onstant.php
@@ -0,0 +1,15 @@
+<?php
+
+define('NUM', 50);
+const NUMBER = 50;
+
+echo NUMBER;
+
+/* 
+
+Constants do not follow any variable scoping rules. They can be defined and accessed from anywhere. Constants name should be all uppercase.
+
+const is a global scope
+variable is global, local, static all scope 
+ 
+ */
