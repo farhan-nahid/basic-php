84e2724540c8927b14d76c375a60f9a879970531
gesora96 <nahidahmed9933@gmail.com>
2022-01-19 23:25:50 +0600

    echo vs print added

---
+++ 3echoPrint.php
@@ -0,0 +1,14 @@
+<?php
+echo 'Hello, ' . 'this' . 'is' . 'Farhan';
+// print 'Hello, '. 'this'. 'is'. 'Farhan'; 
+
+$var = print 'Hello World';
+// $var = echo 'Hello World';
+
+/* 
+
+echo is faster then print. by using echo we can output multiple things but by using print we don't do this. We can't contain echo value in variable because echo doesn't return anything .but print return. And the last things is echo is faster then print. 
+
+by using (.) we can concat string. but by using (+) we can't do this like JS.
+
+*/
