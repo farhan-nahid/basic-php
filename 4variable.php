301952d29b5146db2344d61630920f78a98e12fb
gesora96 <nahidahmed9933@gmail.com>
2022-01-20 00:32:02 +0600

    variable added

---
+++ 4variable.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = 'Farhan Ahmed Nahid';
+$num1 = 20;
+$num2 = 30;
+$sum_of_two_number = $num1 + $num2;
+
+echo "Total Result is: {$sum_of_two_number}";
+
+// when we can want to display variable value we must use double quote 
